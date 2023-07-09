12d236f4e2ac097db613065e00ad31e36a522f27
Rayhan Hasan Labib <87138791+Rayhan-labib7@users.noreply.github.com>
2023-07-10 01:18:48 +0600

    Create generate_real_part_and_imaginary_part.m

---
+++ generate_real_part_and_imaginary_part.m
@@ -0,0 +1,14 @@
+c=-(1/12)*(pi/6)*i;
+K=2;
+n=0:40;
+x=K*exp(c*n);
+subplot(2,1,1);
+stem(n,real(x));
+xlabel('Time index n');
+ylabel('Amplitude');
+title('Real Part');
+subplot(2,1,2);
+stem(n,imag(x));
+xlabel('Time index n');
+ylabel('Amplitude');
+title('imaginary Part');
